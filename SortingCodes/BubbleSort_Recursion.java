83a28a26de9ef3ab4683d66cba30bf13d05668a5
fanfan999 <1058180192@qq.com>
2019-05-03 10:04:48 +0800

    AlgorithmCodes

--- SortingCodes/BubbleSort_Recursion.java
+++ SortingCodes/BubbleSort_Recursion.java
@@ -1,36 +1,47 @@
 package fan;
 
+import java.util.Arrays;
+
+/**
+ * 冒泡排序的递归算法
+ */
+
+//这里递归应该是对圈数递归和单个数组递归
 public class BubbleSort_Recursion {
-    //获取最大值的方法
+    public static void bubbleSort(int[] array, int start, int end) {
+        //判断数组是否有效
+       if (array == null || array.length <= 1) {
+           return;
+       }
 
-    /**
-     *
-     * @param array 要获取最大值的数组
-     * @param L 从哪里开始
-     * @param R 到哪里结束
-     * @return 返回最大值
-     */
-    public static int getMax(int[] array , int L, int R) {
-        //结束条件
-        if (L >= R) {
-            return array[L];
-        }
+        //每一圈数组递归
+       if (start < end - 1) {
+           if (array[start] > array[start + 1]) {
+               swap(start, start + 1, array);
+           }
+           bubbleSort(array, start + 1, end);
+       } else {
+           return;
+       }
 
-        //找到中间位置,划分为左右两边
-        int mid = (L + R) / 2;
+       //圈数递归
+        bubbleSort(array, start, end - 1);
+    }
 
-        //左边最大值
-        int maxLeft = getMax(array, 0, mid);
-        //右边最大值
-        int maxRight = getMax(array, mid + 1, R);
-
-        //返回左右两边最大值
-        return Math.max(maxLeft, maxRight);
+    //交换位置
+    private static void swap(int i, int j, int[] array) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 
     public static void main(String[] args) {
-        int[] array = new int[]{2,3,1,5};
-        int max = getMax(array, 0, array.length - 1);
-        System.out.println(max);
+        int[] array = new int[]{};
+        System.out.println("排序前 : " + Arrays.toString(array));
+        System.out.println(array.length);
+
+        bubbleSort(array,0, array.length);
+
+        System.out.println("排序后 : " + Arrays.toString(array));
     }
 }
